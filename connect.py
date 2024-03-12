e739f785366b033ed341153279c640ab146ead9c
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-12 13:10:04 +0100

    Update

--- connect.py
+++ connect.py
@@ -5,18 +5,19 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-mongo_user = config.get('DB', 'user')
+mongodb_user = config.get('DB', 'user')
 mongodb_pass = config.get('DB', 'pass')
 db_name = config.get('DB', 'db_name')
-# domain = config.get('DB', 'domain')
 
 uri = (
-    f'mongodb+srv://{mongo_user}:{mongodb_pass}'
+    f'mongodb+srv://{mongodb_user}:{mongodb_pass}'
     '@cluster0.xts1vbl.mongodb.net/?retryWrites=true&'
     'w=majority&appName=Cluster0'
 )
 
 
-# Connect to cluster on AtlasDB with connection string
 def create_connect():
+    """
+    Creates a connection with the MongoDB.
+    """
     connect(db=db_name, host=uri, ssl=True)
