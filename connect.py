b3b667190cd979df80ecc498dcf81820ec373314
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-12 19:35:34 +0100

    Update

--- connect.py
+++ connect.py
@@ -1,7 +1,6 @@
 from mongoengine import connect
 import configparser
 
-
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -9,7 +8,7 @@ mongodb_user = config.get('DB', 'user')
 mongodb_pass = config.get('DB', 'pass')
 db_name = config.get('DB', 'db_name')
 
-uri = (
+URI = (
     f'mongodb+srv://{mongodb_user}:{mongodb_pass}'
     '@cluster0.xts1vbl.mongodb.net/?retryWrites=true&'
     'w=majority&appName=Cluster0'
@@ -20,4 +19,4 @@ def create_connect():
     """
     Creates a connection with the MongoDB.
     """
-    connect(db=db_name, host=uri, ssl=True)
+    connect(db=db_name, host=URI, ssl=True)
