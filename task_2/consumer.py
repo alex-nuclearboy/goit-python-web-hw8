380eb94ae952f259be73f6dbc4ea5216872df3cc
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-20 19:19:23 +0100

    Add documentation

--- task_2/consumer.py
+++ task_2/consumer.py
@@ -5,6 +5,13 @@ from models import Contact
 from connect import create_connect
 
 def stub_send_email(contact_id):
+    """
+    Sends an email to a contact based on the provided contact_id.
+
+    param contact_id: The unique identifier of the contact
+                      to whom the email will be sent.
+    return: None
+    """
     contact = Contact.objects(id=contact_id).first()
     if contact:
         contact.message_sent = True
@@ -12,11 +19,24 @@ def stub_send_email(contact_id):
         print(f"Email sent to {contact.fullname} <{contact.email}>")
 
 def callback(ch, method, properties, body):
+    """
+    Callback function that is called when a message is received.
+
+    param ch: The channel object
+    param method: The method object
+    param properties: The properties object
+    param body: The body of the message
+    return: None
+    """
     data = json.loads(body)
     contact_id = data['contact_id']
     stub_send_email(contact_id)
 
 def main():
+    """
+    Main function to establish a connection, declare a queue,
+    and start consuming messages.
+    """
     credentials = pika.PlainCredentials('guest', 'guest')
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='localhost', port=5672, 
