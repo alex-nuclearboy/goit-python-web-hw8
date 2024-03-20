f739b9f23a3e159d71b82fe21e4969e3bc6076af
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-20 19:19:08 +0100

    Add documentation

--- task_2/producer.py
+++ task_2/producer.py
@@ -7,6 +7,12 @@ from connect import create_connect
 fake_data = Faker('en_GB')
 
 def create_fake_contacts(n=10):
+    """
+    Creates fake contacts and saves them to the database.
+
+    :param n: The number of fake contacts to create. Default is 10.
+    :return: None
+    """
     Contact.drop_collection()
     for _ in range(n):
         birthday = fake_data.date_of_birth(minimum_age=18, maximum_age=80)
@@ -22,6 +28,12 @@ def create_fake_contacts(n=10):
 
 
 def send_to_queue(contact_id):
+    """
+    Sends the given contact ID to a queue for further processing.
+
+    param contact_id: The ID of the contact to be sent to the queue.
+    return: None
+    """
     credentials = pika.PlainCredentials('guest', 'guest')
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='localhost', port=5672,
