d4915ff4aabb5702edef79f32846b7d0a9756555
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-22 16:31:18 +0100

    Update

--- task_2/producer.py
+++ task_2/producer.py
@@ -10,9 +10,10 @@ fake_data = Faker('en_GB')
 
 def create_fake_contacts(n=10):
     """
-    Creates fake contacts and saves them to the database.
+    Creates and saves a specified number of fake contacts to the database,
+    and sends their IDs to RabbitMQ queues.
 
-    :param n: The number of fake contacts to create. Default is 10.
+    :param n: The number of fake contacts to create. Defaults to 10.
     :return: None
     """
     Contact.drop_collection()
@@ -20,6 +21,7 @@ def create_fake_contacts(n=10):
         birthday = fake_data.date_of_birth(minimum_age=18, maximum_age=80)
         prefered_method = choice(["email", "sms"])
         message_time = datetime.now()
+
         contact = Contact(
             fullname=fake_data.name(),
             birthday=birthday,
@@ -27,23 +29,25 @@ def create_fake_contacts(n=10):
             email=fake_data.email(),
             phone=fake_data.phone_number(),
             prefered_method=prefered_method,
-            message_status=True,
             message_time=message_time
         )
         contact.save()
+
         send_to_queue(str(contact.id), prefered_method)
 
 
 def send_to_queue(contact_id, prefered_method):
     """
-    Sends the given contact ID to a queue based on the preferred contact method.
+    Sends a contact ID to the appropriate RabbitMQ queue
+    based on the contact's preferred contact method.
 
-    :param contact_id: The ID of the contact to be sent to the queue.
-    :param prefer_method: The preferred method of contacting the user
-                          (email or sms).
-    return: None
+    :param contact_id: The ID of the contact.
+    :param preferred_method: The contact's preferred method of communication
+                             ('email' or 'sms').
+    :return: None
     """
 
+    # Establish a connection to the RabbitMQ server
     credentials = pika.PlainCredentials('guest', 'guest')
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='localhost', port=5672,
@@ -58,11 +62,14 @@ def send_to_queue(contact_id, prefered_method):
         'send_by_email' if prefered_method == 'email' else 'send_by_sms'
     )
 
-    # Send the contact ID to the appropriate queue
+    # Publish the contact ID to the selected queue
     channel.basic_publish(
-        exchange='', routing_key=routing_key,
+        exchange='',
+        routing_key=routing_key,
         body=json.dumps({'contact_id': contact_id}))
+
     print(f" [x] Sent contact ID {contact_id} to {routing_key} queue")
+
     connection.close()
 
 
